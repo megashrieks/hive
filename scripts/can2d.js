72df3735a4c46e8573286ab75d62d19d2cd9411c
acnologia <shrikanthbudya@gmail.com>
2018-04-22 00:42:48 +0530

    basic working.

--- scripts/can2d.js
+++ scripts/can2d.js
@@ -8,13 +8,40 @@ class can2d {
         this.mode = type;
     }
     draw() {
-        ctx[this.mode]();
+        this.ctx[this.mode]();
     }
-    point(x, y, size) {
+    point(p, size) {
         if (size == undefined) size = 1;
-        ctx.beginPath();
-        ctx.arc(x, y, size, 0, Math.PI * 2);
+        this.ctx.beginPath();
+        this.ctx.arc(p.x, p.y, size, 0, Math.PI * 2);
         this.draw();
-        ctx.closePath();
+        this.ctx.closePath();
+    }
+    line(p1, p2) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(p1.x, p1.y);
+        this.ctx.lineTo(p2.x, p2.y);
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+    clear() {
+        this.ctx.clearRect(0, 0, this.can.width, this.can.height);
+    }
+}
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    add(vec) {
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+    clone() {
+        return {
+            x: this.x,
+            y: this.y
+        };
     }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,139 @@ var can = document.getElementById("can");
 var ctx = can.getContext("2d");
 
 var settings = {
-    size: 30
+    size: 200,
+    timeout: 100
 };
+var global_val = {
+    x: ~~(can.width / 2),
+    y: ~~(can.height / 2)
+};
+var creatures = [];
+
 var p = new can2d(can);
+function lerp(a, b, m) {
+    return (1 - m) * a + m * b;
+}
+class Creature {
+    constructor() {
+        var size = settings.size;
+        this.pos = {
+            x: ~~(Math.random() * can.width),
+            y: ~~(Math.random() * can.height)
+        };
+        this.target = {
+            x: ~~(Math.random() * can.width + size),
+            y: ~~(Math.random() * can.height + size)
+        };
+        this.relaxed = false;
+        this.start = {
+            x: 0,
+            y: 0
+        };
+        this.end = {
+            x: 0,
+            y: 0
+        };
+        this.startMid = 0;
+        this.endMid = 0;
+    }
+    set() {
+        this.startMid = 0;
+        this.endMid = 0;
+    }
+    free() {
+        var a = this;
+        setTimeout(function() {
+            a.relaxed = false;
+        }, settings.timeout);
+    }
+    setTarget() {
+        var x_diff = global_val.x - this.pos.x;
+        var y_diff = global_val.y - this.pos.y;
+        if (Math.abs(x_diff) > Math.abs(y_diff)) {
+            if (x_diff < 0) this.target.x -= settings.size;
+            else this.target.x += settings.size;
+        } else {
+            if (y_diff < 0) this.target.y -= settings.size;
+            else this.target.y += settings.size;
+        }
+    }
+    update() {
+        if (this.relaxed) return;
+        if (this.endMid <= 1.0) {
+            this.end = {
+                x: lerp(this.pos.x, this.target.x, this.endMid),
+                y: lerp(this.pos.y, this.target.y, this.endMid)
+            };
+            this.endMid += 0.05;
+        } else {
+            if (this.startMid <= 1.0) {
+                this.start = {
+                    x: lerp(this.pos.x, this.target.x, this.startMid),
+                    y: lerp(this.pos.y, this.target.y, this.startMid)
+                };
+                this.startMid += 0.05;
+            } else {
+                this.relaxed = true;
+                this.pos = Object.assign({}, this.target);
+                this.set();
+                this.free();
+                this.setTarget();
+            }
+        }
+    }
+    draw() {
+        if (this.relaxed) {
+            p.setType("stroke");
+            ctx.fillStyle = "rgb(0,255,255)";
+            p.point(this.pos, 5);
+            ctx.fillStyle = "#000";
+        } else {
+            p.line(this.start, this.end);
+        }
+    }
+}
+
 function createGrid() {
     var size = settings.size;
     ctx.fillStyle = "#000";
-    for (var i = size; i < can.width; i += size) {
-        for (var j = size; j < can.height; j += size) {
-            p.point(i, j, 2);
+    for (var i = 0; i < can.width; i += size) {
+        for (var j = 0; j < can.height; j += size) {
+            var v = new Vector(i, j);
+            p.point(v, 2);
         }
     }
 }
 function resize() {
     can.width = window.innerWidth;
     can.height = window.innerHeight;
+    global_val = {
+        x: can.width / 2,
+        y: can.height / 2
+    };
+}
+function setup() {
+    creatures = [];
+    for (var i = 0; i < 10; ++i) {
+        creatures.push(new Creature());
+    }
+}
+function draw() {
+    createGrid();
+    p.clear();
+    for (var i = 0; i < creatures.length; ++i) {
+        creatures[i].update();
+        creatures[i].draw();
+    }
+    requestAnimationFrame(draw);
 }
 window.onresize = resize;
 resize();
-createGrid();
+setup();
+can.onmousemove = function(e) {
+    global_val = {
+        x: e.clientX,
+        y: e.clientY
+    };
+};
+requestAnimationFrame(draw);
